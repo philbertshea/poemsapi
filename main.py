fe484b446f1a17f28a3247b50634f84020ad0205
Philbert Shea Pok Him <philbertsph@phillip.com.sg>
2024-06-13 13:54:45 +0800

    Replicate yt

--- main.py
+++ main.py
@@ -1,41 +1,32 @@
-from selenium.webdriver.chrome.options import Options
-from bs4 import BeautifulSoup
-from flask import Flask, request
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from flask import Flask, request
 from selenium.webdriver.chrome.service import Service
-import json
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
 app = Flask(__name__)
 
-def scrape_poems():
+
+def download_selenium():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    prefs = {"profile.managed_default_content_settings.images": 2}
+    chrome_options.add_experimental_option("prefs", prefs)
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-    
-    ROOT = "https://smart.poems.com.sg/smartpark/"
-    website = ROOT
-    driver.get(website)
-
-    box = driver.find_element(By.CSS_SELECTOR, "div[class='cpark']").find_element(By.CSS_SELECTOR, "div[class='boxedcontent']")
-    section = box.find_element(By.CSS_SELECTOR, "section[class='introblock whites brow-section calltoactionarea page_relative ware-of-invest']")
-    return_div = section.find_element(By.CSS_SELECTOR, "div[class='mob']")
-    sgd = return_div.find_element(By.XPATH, ".//p[5]").text
-    usd = return_div.find_element(By.XPATH, ".//p[6]").text
-    return {
-        "sgd": sgd,
-        "usd": usd
-    }
+    driver.get("https://google.com")
+    title = driver.title
+    language = driver.find_element(By.XPATH, "//div[@id='SIvCob']").text
+    data = {'Page Title': title, 'Language': language}
+    return data
 
 
-@app.route("/", methods=['GET', 'POST'])
-def get_data():
+@app.route('/', methods = ['GET','POST'])
+def home():
     if (request.method == 'GET'):
-        result = scrape_poems()
-        return result
+        return download_selenium()
 
-if __name__ == '__main__':
-    app.run(port=7777)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3000)
